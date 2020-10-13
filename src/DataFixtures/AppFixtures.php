8bfd63948aa7f5dea977ab14f6ba6d0170d00085
basebro <christianacevedo.89@gmail.com>
2020-10-13 04:28:47 +0200

    adding role like array and encoding password correctly

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,16 +5,24 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
         $user->setName('Admin');
         $user->setUsername('admin');
-        $user->setPassword('adminpassword');
-        $user->setRoles('ADMIN');
+        $user->setPassword($this->encoder->encodePassword($user, 'adminpassword'));
+        $user->setRoles(['ROLE_ADMIN']);
         $manager->persist($user);
 
         $manager->flush();
